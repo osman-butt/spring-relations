540f4d1e602be9e378b6eae2616e29891747fd87
osman-butt <butt.osman@gmail.com>
2024-02-16 15:58:53 +0100

    Orders controller

---
+++ src/main/java/kea/exercise/orderexercise/controller/OrderController.java
@@ -0,0 +1,28 @@
+package kea.exercise.orderexercise.controller;
+
+import kea.exercise.orderexercise.model.Order;
+import kea.exercise.orderexercise.repository.OrderRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+}
