7243153831cf05892b50056f9daded0af7c2a5ff
osman-butt <butt.osman@gmail.com>
2024-02-16 14:40:34 +0100

    Added Post /OrderLines

--- src/main/java/kea/exercise/orderexercise/controller/OrderLinesController.java
+++ src/main/java/kea/exercise/orderexercise/controller/OrderLinesController.java
@@ -2,9 +2,7 @@ package kea.exercise.orderexercise.controller;
 
 import kea.exercise.orderexercise.model.OrderLine;
 import kea.exercise.orderexercise.repository.OrderLineRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,10 @@ public class OrderLinesController {
     @GetMapping
     public List<OrderLine> getAllOrderLines() {
         return orderLineRepository.findAll();
+    }
+
+    @PostMapping
+    public OrderLine createOrderLine(@RequestBody OrderLine orderLine) {
+        return orderLineRepository.save(orderLine);
     }
 }
